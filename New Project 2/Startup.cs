c288dba5c0d6b952f2ae5d87ad4c9d599f19db98
chrispratt1 <chrisprattwork95@gmail.com>
2016-12-06 18:41:06 -0700

    initial commit

---
+++ New Project 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(New_Project_2.Startup))]
+namespace New_Project_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
